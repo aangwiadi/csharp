76585bc25db3da0eb8b0d54a256a95d6dbdf2a7a
aangwiadi <boang24@gmail.com>
2017-11-12 01:25:31 +0700

    perubahan setting dan doc

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
+
+	// configurasi database
   define('HOST','');
   define('USER','');
   define('PASS','');
   define('DB','dbcrud_dekstop');
+  
+  
   $conn = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 
+  // set default time zone
   date_default_timezone_set("Asia/Jakarta");
 ?>
